f1ddeab5d30c495336d62479f6ebdb846e42ffb0
Aman Sharma <amansharma3009@hotmail.com>
2021-06-10 16:40:19 +0530

    Hellow World

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require 'common/header.html';?>
+	   <div class="container">
+	   	<div class="image">
+	  <img src="images/hello.png" />
+	</div>
+	<div class="data">
+	<h1>Hello World</h1>
+</div>
+	</div>
+<?php require 'common/footer.html';?>
